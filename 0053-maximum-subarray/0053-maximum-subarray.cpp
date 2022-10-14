20d91261fe8cec676868b8cc92f210545cab35c6
Alambek Gulamidinov <77918145+Heox1@users.noreply.github.com>
2022-10-14 17:49:17 +0900

    Time: 143 ms (78.45%), Space: 68.1 MB (7.75%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int mx = -99999, sm = 0;
-        for(auto i : nums){
-            sm += i;
-            if(sm > mx) mx = sm;
-            if(sm < 0) sm = 0;
-        }
-        return mx;
+        int n = nums.size();
+        
+        int dp[n + 10];
+        dp[0] = nums[0];
+        int ans = dp[0];
+        for(int i = 1;i < n;i ++){
+            dp[i] = nums[i] + max(dp[i - 1], 0);
+            ans = max(ans, dp[i]);
+        } 
+        return ans;
     }
 };
