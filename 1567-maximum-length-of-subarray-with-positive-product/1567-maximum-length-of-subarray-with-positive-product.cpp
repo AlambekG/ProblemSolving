ab77c2e8efcbe34e095e4bb9aa025a2af04db834
Alambek Gulamidinov <77918145+Heox1@users.noreply.github.com>
2022-10-23 19:30:07 +0900

    Time: 321 ms (22.78%), Space: 57.9 MB (76.65%) - LeetHub

--- 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.cpp
+++ 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.cpp
@@ -1,63 +1,15 @@
 class Solution {
 public:
-    int getMaxLen(vector<int>& nums) {
-        int n = nums.size() + 10;
-        
-        long int dp[n][2], d[n][2];
-        
-        if(nums[0] < 0) {
-            dp[0][0] = 0;
-            dp[0][1] = nums[0];
-            d[0][0] = 0;
-            d[0][1] = 1;
-        }
-        else if(nums[0] == 0){
-             dp[0][0] = 0;
-            dp[0][1] = 0;
-            d[0][0] = 0;
-            d[0][1] = 0;
-        }
-        else{
-            dp[0][0] = nums[0];
-            dp[0][1] = 0;
-            d[0][0] = 1;
-            d[0][1] = 0;
-        }
-        
-        long int mx = d[0][0];
-        for(int i = 1;i < nums.size();i ++){
-            if(nums[i] < 0){
-                if(nums[i] * dp[i - 1][1] > 0){
-                    dp[i][0] = 3; 
-                    d[i][0] = d[i - 1][1] + 1;
-                }
-                else{
-                    dp[i][0] = 0;
-                    d[i][0] = 1;
-                } 
-                if(dp[i - 1][0] > 0){
-                    dp[i][1] = -3;
-                    d[i][1] = d[i - 1][0] + 1;
-                }
-                else{
-                    dp[i][1] = nums[i];
-                    d[i][1] = 1;
-                }
-            }
-            else if(nums[i] == 0){
-                dp[i][0] = dp[i][1] = d[i][0] = d[i][1] = 0;
-            }
-            else{
-                if(dp[i - 1][0] > 0) d[i][0] = d[i - 1][0] + 1;
-                else d[i][0] = 1;
-                if(dp[i - 1][1] < 0) d[i][1] = d[i - 1][1] + 1;
-                else d[i][1] = 1;
-                dp[i][0] = 3;
-                if(dp[i - 1][1] == 0) dp[i][1] = 0;
-                else dp[i][1] = -3;
-            }
-            mx = max(mx, d[i][0]);
-        }
-        return int(mx);
+    int getMaxLen(vector<int>& nums) {    int posLen = 0, negLen = 0, maxLen = 0;
+	for (int i : nums) {
+		if (i == 0) posLen = 0, negLen = 0;
+		else {
+			if (i < 0) swap(posLen, negLen);
+			if (posLen > 0 || i > 0) ++posLen;
+			if (negLen > 0 || i < 0) ++negLen;
+			maxLen = max(maxLen, posLen);
+		}
+	}
+	return maxLen;
     }
 };
