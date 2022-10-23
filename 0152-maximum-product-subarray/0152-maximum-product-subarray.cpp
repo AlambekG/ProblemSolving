f05cec8c188842eec6b075e7b7f0823887a22a78
Alambek Gulamidinov <77918145+Heox1@users.noreply.github.com>
2022-10-23 19:23:16 +0900

    Time: 122 ms (5.03%), Space: 14.1 MB (7.95%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n = nums.size() + 10;
+        
+        long long dp[n][2];
+        
+        if(nums[0] < 0) {
+            dp[0][0] = 0;
+            dp[0][1] = nums[0]; 
+        }
+        else if(nums[0] == 0){
+             dp[0][0] = 0;
+            dp[0][1] = 0; 
+        }
+        else{
+            dp[0][0] = nums[0];
+            dp[0][1] = 0; 
+        }
+        
+        long long mx = nums[0];
+        for(int i = 1;i < nums.size();i ++){
+            if(nums[i] < 0){
+                if(nums[i] * dp[i - 1][1] > 0) 
+                    dp[i][0] = nums[i] * dp[i - 1][1]; 
+                else dp[i][0] = 0;
+                if(dp[i - 1][0] > 0) dp[i][1] = dp[i - 1][0] * nums[i];
+                else dp[i][1] = nums[i];
+            }
+            else if(nums[i] == 0){
+                dp[i][0] = dp[i][1] = 0;
+            }
+            else{
+                dp[i][0] = nums[i];
+                if(dp[i - 1][0] > 0) dp[i][0] *= dp[i - 1][0];
+                if(dp[i - 1][1] != 0)   
+                    dp[i][1] = dp[i - 1][1] * nums[i];
+                else dp[i][1] = 0;
+            }
+            mx = max(mx, dp[i][0]);
+            cout << dp[i][0] << ' ' << dp[i][1] << endl;
+    }
+        return mx;
+    }
+};
