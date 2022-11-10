8b748a26887bd8c86936773f544e3067caed3381
Alambek Gulamidinov <77918145+Heox1@users.noreply.github.com>
2022-11-11 08:54:10 +0900

    Time: 38 ms (69.55%), Space: 11 MB (66.48%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     string removeDuplicates(string s) {
-        int l = 0;
         
-        unordered_map <int, int> mp;
-        for(int i = 1;i < s.size();i ++){
-            if(l != i && l > -1 && s[l] == s[i]){
-                mp[l] = mp[i] = 1;
-                while(mp[l]) l --;
-            }
-            else l = i;
+        string ans;
+        
+        for(int i = 0 ; i < s.size() ; i++)
+        {
+            if(ans.size() && ans.back() == s[i])
+                ans.pop_back();
+            else
+                ans.push_back(s[i]);
         }
         
-        string a;
-        for(int i = 0;i < s.size();i ++)
-            if(!mp[i]) a += s[i];
-        return a;
+        return ans;
     }
 };
