8192c6c21860dfc843478cced9d277486073764d
Alambek Gulamidinov <77918145+Heox1@users.noreply.github.com>
2022-11-14 17:38:47 +0900

    Time: 93 ms (98.10%), Space: 45.3 MB (64.97%) - LeetHub

--- 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.cpp
+++ 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.cpp
@@ -18,14 +18,11 @@ public:
         ss[n - 1] = v[n - 1].second;
         for(int i = n - 2;i >= 0;i --){
             ss[i] = ss[i + 1] + v[i].second;
-        }
-        
+        }  
         pref[0] = 0;
         for(int i = 1;i < n;i ++){
             pref[i] = pp[i - 1] * (v[i].first - v[i - 1].first) + pref[i - 1];     
         }
-        
-        
         suf[n - 1] = 0;
         for(int i = n - 2;i >= 0;i --){
             suf[i] = ss[i + 1] * (v[i + 1].first - v[i].first) + suf[i + 1];
