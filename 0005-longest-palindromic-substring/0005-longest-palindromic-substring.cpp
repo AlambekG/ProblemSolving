5485829570d81f4714b01c8b0a06d12d260606e6
Alambek Gulamidinov <77918145+Heox1@users.noreply.github.com>
2022-11-22 04:54:20 +0900

    Time: 19 ms (84.60%), Space: 7.3 MB (66.37%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
-    string even(string s){
+    string fun(string s, int cnt){
         string ans = "";
         
-        int sz = 1;
+        int sz = cnt;
         
         pair <int, int> pp;
         int mx = 0;
@@ -24,35 +24,10 @@ public:
         
         return ans;
     }
-    string odd(string s){
-        string ans = "";
-        
-        int sz = 2;
-        
-        pair <int, int> pp;
-        int mx = 0;
-        
-        for(int i = 0;i < s.size();i ++){
-            int r = i + sz, l = i;
-             while(r < s.size() && l > -1 && s[l] == s[r]){
-                 if(r - l + 1 > mx){
-                     mx = r - l + 1;
-                     pp = make_pair(l, r);
-                 }
-                 r ++;
-                 l --;
-             }
-        }
-        for(int i = pp.first;i <= pp.second;i ++)
-             ans += s[i];
-        
-        return ans;
-    }
-    
     
     string longestPalindrome(string s) {
-         string ans1 = even(s);
-         string ans2 = odd(s);
+         string ans1 = fun(s, 1);
+         string ans2 = fun(s, 2);
         
          if(ans1.size() > ans2.size()) return ans1;
          return ans2;
