4e6719afd3a84e78a24f0c5c1bf36433535db53b
Alambek Gulamidinov <77918145+Heox1@users.noreply.github.com>
2022-11-27 23:48:34 +0900

    Time: 129 ms (91.31%), Space: 38.5 MB (85.95%) - LeetHub

--- 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.cpp
+++ 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.cpp
@@ -1,35 +1,23 @@
 class Solution {
 public:
     long long minCost(vector<int>& nums, vector<int>& cost) {
-        vector <pair <int, int> > v;
-        
-        for(int i = 0;i < nums.size();i ++){
-            v.push_back(make_pair(nums[i], cost[i]));
-        }
-        sort(v.begin(), v.end());
-        
-        int n = nums.size();
-        long long pp[n + 10], ss[n + 1], pref[n + 10], suf[n + 10];
-        
-        pp[0] = v[0].second;
-        for(int i = 1;i < n;i ++){
-            pp[i] = pp[i - 1] + v[i].second;
-        }
-        ss[n - 1] = v[n - 1].second;
-        for(int i = n - 2;i >= 0;i --){
-            ss[i] = ss[i + 1] + v[i].second;
-        }  
-        pref[0] = 0;
-        for(int i = 1;i < n;i ++){
-            pref[i] = pp[i - 1] * (v[i].first - v[i - 1].first) + pref[i - 1];     
-        }
-        suf[n - 1] = 0;
-        for(int i = n - 2;i >= 0;i --){
-            suf[i] = ss[i + 1] * (v[i + 1].first - v[i].first) + suf[i + 1];
-        }
-        long long ans = suf[0];
-        for(int i = 0;i < n;i ++){
-            ans = min(ans, pref[i] + suf[i]);
+        int l = *min_element(nums.begin(),nums.end());
+        int r = *max_element(nums.begin(),nums.end());
+        long long ans = 1e18;
+        while (r >= l) {
+            long long mid1 = l + (r - l) / 3;
+            long long mid2 = r - (r - l) / 3;
+            
+            long long a1 = 0, a2 = 0;
+            
+            for(int i=0;i<nums.size();i++){
+                a1 += (abs(mid1 - nums[i]) * cost[i]);
+                a2 += (abs(mid2 - nums[i]) * cost[i]);
+            }
+            
+            if(a1 < a2) r = mid2-1;
+            else l = mid1+1;
+			ans = min({ans,a1,a2});
         }
         return ans;
     }
