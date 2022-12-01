13e239757f8582d94da35093d90014fca71d62c6
Alambek Gulamidinov <77918145+Heox1@users.noreply.github.com>
2022-12-02 00:45:22 +0900

    Time: 174 ms (70.21%), Space: 28.7 MB (52.03%) - LeetHub

--- 2484-count-palindromic-subsequences/2484-count-palindromic-subsequences.cpp
+++ 2484-count-palindromic-subsequences/2484-count-palindromic-subsequences.cpp
@@ -4,35 +4,28 @@ public:
     int dk[10020][11];
     long long dp[10020][11][11];  
     
-    int dk2[10020][11];
+    int dk2[10020][11], mod = 1e9 + 7, x;
     long long dp2[10020][11][11]; 
-    
-    int mod = 1e9 + 7;
-    
     
     int countPalindromes(string s) {
         if(s.size() < 5) return 0;
         
         for(int i = 0;i < s.size();i ++){
-            int x = int(s[i] - 48);
+            x = int(s[i] - 48);
             dk[i][x] = 1;
-            
             for(int k = 0;k < 10;k ++){
                 if(i > 0)
                     for(int c = 0;c < 10;c ++)
                         dp[i][k][c] = dp[i - 1][k][c];
-                
                 if(i > 0){
                     dk[i][k] += dk[i - 1][k];
                     dp[i][k][x] += dk[i - 1][k];  
                 } 
             }
         }
-        
         for(int i = s.size() - 1;i >= 0;i --){
-            int x = int(s[i] - 48);
-            dk2[i][x] = 1;
-            
+            x = int(s[i] - 48);
+            dk2[i][x] = 1;  
             for(int k = 0;k < 10;k ++){
                 for(int c = 0;c < 10;c ++)
                     dp2[i][k][c] = dp2[i + 1][k][c];
@@ -40,27 +33,15 @@ public:
                 dp2[i][k][x] += dk2[i + 1][k];  
             }
         }
-        
-        // for(int i = 0;i < s.size();i ++)
-        //     cout << dp[i][0][1] << ' ';
-        // cout << endl;
-        // for(int i = 0;i < s.size();i ++)
-        //     cout << dp2[i][0][1] << ' ';
-        
-        int cnt = 0;
-        
+        int cnt = 0;        
         for(int i = 2;i < s.size() - 2;i ++){
             long long val = 0;
-            
-            // cout << dp[i - 1][1][0] << ' ' << dp2[i + 1][1][0] << endl;
             for(int k = 0;k < 10;k ++){
                 for(int c = 0;c < 10;c ++){
                     val += (dp[i - 1][k][c] * dp2[i + 1][k][c]) % mod;
                 }
             }
-
             cnt = (cnt + val) % mod;
-            // cout << "cnt: " << cnt << ' ' << val << endl;
         }
         
         return cnt;
